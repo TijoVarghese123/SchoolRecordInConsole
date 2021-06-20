55c18aac9e2ea08caed61c1dad1bbfa33900dcfe
TijoVarghese123 <tijokallanani@gmail.com>
2021-06-20 20:50:59 +0530

    initial commit

---
+++ SchoolRecoedConsole/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace SchoolRecoedConsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number=1;
+            int AdmNo = 0;
+            string Name = "";
+            int Division = 0;
+            Student stud = new Student();
+            while (Convert.ToInt32( number)<5)
+            {
+                Console.WriteLine("Enter a Number");
+                Console.WriteLine("1- Add Record");
+                Console.WriteLine("2- Edit Record");
+                Console.WriteLine("3- Delete Record");
+                Console.WriteLine("4- Exit");
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number==1)
+                {
+                    
+                    Console.WriteLine("Enter Admission Number");
+                    AdmNo = Convert.ToInt32(Console.ReadLine());
+                    string checkId=stud.CheckId(AdmNo);
+                    if(checkId == "")
+                    {
+                        Console.WriteLine("Enter Your Name");
+                        Name = Console.ReadLine();
+                        Console.WriteLine("Enter Your Mark");
+                        Division = Convert.ToInt32(Console.ReadLine());
+                        var arlist1 = new ArrayList();
+                        arlist1.Add(Name);
+                        arlist1.Add(Division);
+                        stud.SaveData(AdmNo, arlist1);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine( "Admission Number Already Exists");
+                        Console.WriteLine("==================================");
+                        stud.DisplayData();
+                    }
+               
+                }
+                if(number==2)
+                {
+                    Console.WriteLine("Enter Admission number");
+                    int EditAdm = Convert.ToInt32(Console.ReadLine());
+                    stud.EditData(EditAdm);
+                }
+                if(number==3)
+                {
+                    Console.WriteLine("Enter Admission number");
+                    int DeletAdm = Convert.ToInt32(Console.ReadLine());
+                    stud.DeleteData(DeletAdm);
+                }
+
+                if(number==4)
+                {
+                    string confirm;
+                    Console.Write("Sure You Want to Quit press Y/N  ");
+                    confirm = Console.ReadLine();
+                    if(confirm=="Y" || confirm=="y")
+                    {
+                       
+                        return;
+
+                    }
+                    else if (confirm == "N" || confirm == "n")
+                    {
+                        Console.WriteLine("Continue Process");
+                        Console.WriteLine("==================================");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Operation");
+                        Console.WriteLine("==================================");
+                    }
+
+                }               
+            }   
+        }
+    }    
+}
+
+
---
+++ SchoolRecoedConsole/Student.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace SchoolRecoedConsole
+{
+    class Student
+    {
+        Dictionary<int, ArrayList> numberNames = new Dictionary<int, ArrayList>();
+        public void SaveData(int id,ArrayList data)
+        {
+             
+                numberNames.Add (id, data);
+                Console.WriteLine();
+                Console.WriteLine("Added Successfully");
+                Console.WriteLine("==================================");
+                DisplayData();
+            
+        }
+        public void EditData(int id)
+        {
+            if (numberNames.ContainsKey(id))
+            {
+                ArrayList arlist = new ArrayList();
+
+                if (numberNames.TryGetValue(id, out arlist))
+                {
+                    
+                    Console.Write("Name is [{0}] ",arlist[0]);
+                    string name = Console.ReadLine();
+                    if (name=="")
+                    {
+                        name = arlist[0].ToString();
+                    }
+                    Console.Write("Mark is [{0}] " , arlist[1]);
+                    string mark =Console.ReadLine();
+                    if (mark=="")
+                    {
+                        mark = arlist[1].ToString(); 
+                    }
+                    int ExamMark = Convert.ToInt32(mark);
+                    Console.WriteLine();
+                    Console.WriteLine("Updated Successfully");
+                    Console.WriteLine("==================================");
+                   // ArrayList list = new ArrayList();
+                    arlist.Clear();
+                    arlist.Add(name);
+                    arlist.Add(ExamMark);
+                    numberNames[id] = arlist;
+                    DisplayData();     
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("No key found");
+                Console.WriteLine("==================================");
+            }
+        }
+
+
+        public void DisplayData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=================================");
+            Console.WriteLine("AdmNo\tName\tMarks");
+            Console.WriteLine("=================================");
+
+            foreach (var i in numberNames)
+            {
+
+                Console.Write(i.Key);
+                Console.Write("\t");
+                Console.Write(i.Value[0]);
+                Console.Write("\t");
+                Console.WriteLine(i.Value[1]);
+            }
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+        }
+
+        public void DeleteData(int id)
+        {
+            if (numberNames.ContainsKey(id))
+            {
+
+                string confirm;
+                Console.Write("Sure You Want to Delete- press Y/N  ");
+                confirm = Console.ReadLine();
+                if (confirm == "Y" || confirm == "y")
+                {
+                    numberNames.Remove(id);
+                    Console.WriteLine();
+                    Console.WriteLine("Deleted Successfully");
+                    Console.WriteLine("==================================");
+                    DisplayData();
+                }
+                else if (confirm == "N" || confirm == "n")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Cancelled");
+                    Console.WriteLine("==================================");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Operation");
+                    Console.WriteLine("==================================");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("No key found");
+                Console.WriteLine("==================================");
+            }
+        }
+
+        public  string CheckId(int id)
+        {
+            string s="";
+            if (numberNames.ContainsKey(id))
+            {
+                
+                Console.WriteLine();
+                s="Admission Number Already present";
+                Console.WriteLine("==================================");
+                
+            }
+            return s;
+        }
+    }
+}
